6a70381e6f889211a6f8ef91be94d973b542486b
rafael <support@tierrasky.com>
2018-04-11 15:54:38 -0400

    Adjusted the HomePage with LazyLoading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  	pages;
-	  rootPage;
+  	// pages;
+	  rootPage: string = 'HomePage';
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -21,14 +20,13 @@ export class MyApp {
 
 
     // set our app's pages
-		this.pages = [
-			{ title: 'Home', component: HomePage, icon: 'home' },
+		// this.pages = [
+		// 	{ title: 'Home', component: HomePage, icon: 'home' },
 			// { title: 'Slides', component: SlideBoxPage, icon: 'swap' },
 			// { title: 'Google maps', component: GoogleMapsPage, icon: 'map' },
 			// { title: 'Components', component: ComponentsListPage, icon: 'grid' }
-		];
+		// ];
 
-    this.rootPage = HomePage;
   }
 }
 
